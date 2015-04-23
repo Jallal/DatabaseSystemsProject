93341ad04b5019dbd002fd3775f9eda93b597fe4
SuperMex <adelinezzm@gmail.com>
2015-04-23 13:23:17 -0400

    added functions for updating document and getting document tree

--- lib/cls/Documents.php
+++ lib/cls/Documents.php
@@ -59,20 +59,80 @@ SQL;
         }
         foreach($statement as $row) {
             $count = $row['COUNT'];
-
         }
 
         return  $count;
     }
 
-    public function createDocument($name, $projid, $projownerid, $creatorid) {
+    public function createDocument($name, $projid, $projownerid, $creatorid, $content) {
         $sql = <<< SQL
-INSERT INTO $this->tableName (ProjID, ProjOwnerID, creatorID, Filename, versionNo, create_time)
+SELECT * from $this->tableName
+where ProjID=? and Filename=?
+SQL;
+        $statement = $this->pdo()->prepare($sql);
+        $statement->execute(array($projid, $name));
+
+        if ($statement->rowCount() > 0) {
+            return false;
+        }
+
+        $sql = <<< SQL
+INSERT INTO $this->tableName (ProjID, ProjOwnerID, creatorID, Filename, versionNo, create_time, content)
 values (?, ?, ?, ?, ?, ?)
 SQL;
 
         $statement = $this->pdo()->prepare($sql);
-        $statement->execute(array($projid, $projownerid, $creatorid, $name, 1, date("Y-m-d H:i:s")));
+        $statement->execute(array($projid, $projownerid, $creatorid, $name, 1, date("Y-m-d H:i:s"), $content));
     }
 
+    public function updateDocument($name, $projid, $userid, $content) {
+        $sql = <<< SQL
+SELECT * from $this->tableName
+where ProjID=? and Filename=?
+ORDER BY versionNo DESC
+LIMIT 1
+SQL;
+
+        $pdo = $this->pdo();
+        $statement = $pdo->prepare($sql);
+        $statement->execute(array($projid, $name));
+
+        if($statement->rowCount() === 0) {
+            return false;
+        }
+        $version = 0;
+        $projownerid = null;
+        $parentid = null;
+        foreach($statement as $row) {
+            $projownerid = $row['ProjOwnerID'];
+            $version = $row['versionNo'] + 1;
+            $parentid = $row['DocID'];
+        }
+
+        $sql = <<<SQL
+INSERT INTO $this->tableName (ProjID, ProjOwnerID, creatorID, Filename, versionNo, create_time, parentDocID, content)
+values (?,?,?,?,?,?,?)
+SQL;
+
+        $statement = $this->pdo()->prepare($sql);
+        $statement->execute(array($projid, $projownerid, $userid, $name, $version, date("Y-m-d H:i:s"), $parentid, $content));
+    }
+
+    public function getDocTree($name, $projid) {
+        $sql = <<<SQL
+SELECT * from $this->tableName
+where ProjID=? and Filename=?
+ORDER BY versionNo DESC
+SQL;
+
+        $statement = $this->pdo()->prepare($sql);
+        $statement->execute(array($projid, $name));
+
+        $result = array();
+        foreach($statement as $row) {
+            $result[] = new Document($row);
+        }
+
+        return $result;
+    }
 }
